51995c8ab471fff0ec9a5e83744ce102370d3c8a
NahiyaNasir <nahiaesha.01@gmail.com>
2024-09-26 20:39:43 +0600

    Cr:admin

---
+++ Admin/isAdmin.js
@@ -0,0 +1,18 @@
+ const User=require('../User/userSchema')
+
+
+const User = require('../models/User');
+
+// Middleware to check if the user is an admin
+const isAdmin = async (req, res, next) => {
+  const userId = req.user.id; // Assuming the user ID is set after authentication
+  const user = await User.findById(userId);
+
+  if (user && user.role === 'admin') {
+    next(); // If the user is an admin, proceed
+  } else {
+    return res.status(403).json({ message: 'Access denied, admin only.' });
+  }
+};
+
+module.exports = { isAdmin };
